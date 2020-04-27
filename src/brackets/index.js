e491fa9e66a1724605353e0cb39a60ce8506ecb1
Timring <timringperez@gmail.com>
2020-04-27 09:34:27 +0100

    add countOpenBrackets and countCloseBrackets functions

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,35 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+  str = [...str];
+  let openBrackets = ["(", "[", "{"];
+  let closeBrackets = [")", "]", "}"];
+  let numberOfOpenBrackets = {};
+  let numberOfCloseBrackets = {};
+
+  function countOpenBrackets() {
+    for(let i = 0; i < str.length; i++) {
+      if(openBrackets.includes(str[i])) {
+        (!numberOfOpenBrackets[`${str[i]}`]) ?
+        numberOfOpenBrackets[`${str[i]}`] = 1 
+        : numberOfOpenBrackets[`${str[i]}`]++;
+      }
+    }
+  }
+
+  function countCloseBrackets() {
+    for(let i = 0; i < str.length; i++) {
+      if(closeBrackets.includes(str[i])) {
+        (!numberOfCloseBrackets[`${str[i]}`]) ?
+        numberOfCloseBrackets[`${str[i]}`] = 1 
+        : numberOfCloseBrackets[`${str[i]}`]++;
+      }
+    }
+  }
+
+
+
+}
 
 module.exports = isValid;
