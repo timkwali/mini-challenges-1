9d1741c7c582e7958f09ef3423dd7a99d7d481fd
Timring <timringperez@gmail.com>
2020-05-04 12:11:32 +0100

    create output variable and transpose elements into output

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,19 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  let output = [];
+  for(let i = 0; i < array.length; i++) {
+    let currentArray = array[i];
+    for(let j = 0; j < currentArray.length; j++) {
+      if(i === 0) {
+        output.push([currentArray[j]]);
+      } else {
+        output[j].push(currentArray[j]);
+      }
+    }
+  }
+
+}
 
 module.exports = transpose;
